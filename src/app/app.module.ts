0e36d796989983488ce78ea3f2319fadbc9e77a3
Patryk Klej <patryk.klej00@gmail.com>
2022-01-05 14:48:55 +0100

    NavBar oraz app module z komponentami

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { ShowingsComponent } from './showings/showings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    RoomsComponent,
+    ShowingsComponent
   ],
   imports: [
     BrowserModule
